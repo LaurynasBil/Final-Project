c2d4d89779ad084ca8f97bb12f38572df1f0a651
Laurynas Bil <laurynas.bilziukevicius@gmail.com>
2023-12-12 15:28:20 +0200

    Scraping done
    Data ahs been scraped and stored in a CSV file to get to the data faster

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Test')
+import pandas as pd
+from scraping import scrape
+
+# df1 = scrape('https://www.imdb.com/list/ls063676189/?st_dt=&mode=detail&page=')
+# df2 = scrape('https://www.imdb.com/list/ls063676660/?st_dt=&mode=detail&page=')
+# result = pd.concat([df1, df2])
+# result.to_csv("result.csv", index = False)
+df = pd.read_csv('result.csv')
